365dafe580c95f86fd4e8bc5064a74d05e35e1a3
DESKTOP-SJ4L2R5\Admin <hmduc197@gmail.com>
2021-04-04 01:23:36 +0700

    first commit

---
+++ app/src/main/java/com/hmduc/qrcodeapp/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.hmduc.qrcodeapp
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log.d
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
+import com.google.zxing.Result
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val CAMERA_REQUEST_CODE= 101
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var codeScanner: CodeScanner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        codeScanner = CodeScanner(this, codeScannerView)
+        codeScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+
+            decodeCallback = DecodeCallback {
+                runOnUiThread{
+                    text.text = it.text
+                }
+            }
+
+            errorCallback = ErrorCallback {
+                runOnUiThread{
+                    d("Error:", it.message.toString())
+                }
+            }
+        }
+
+        codeScannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onRestart() {
+        codeScanner.releaseResources()
+        super.onRestart()
+    }
+
+    private fun setUpPermission() {
+        val permission = ContextCompat.checkSelfPermission(this,android.Manifest.permission.CAMERA)
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            makeRequestPermission()
+        }
+    }
+
+    private fun makeRequestPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA),
+            CAMERA_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "You need the camera permission to be able to use this App!",Toast.LENGTH_SHORT)
+                }
+            }
+        }
+    }
+}
